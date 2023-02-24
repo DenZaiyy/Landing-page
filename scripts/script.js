9927da0ccdf460d4a849c1134defa51b62f0807e
DenZaiyy <denzaiyy@outlook.fr>
2023-02-24 17:33:48 +0100

    Adding a settings with colors change and adjust page

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,3 @@
-const infos = [
-    {
-        icon: "fa-globe",
-        name: "Digital Design",
-        desc: "Some quick example text to build on the card title and make up the bulk of the card the platform.",
-    },
-    {
-        icon: "fa-brush",
-        name: "Unlimited Colors",
-        desc: "Credibly brand stanaads compliant user exteible services College Anibh ocean euismod tincidunt looreet.",
-    },
-    {
-        icon: "fa-chess",
-        name: "Strategy Solutions",
-        desc: "Separated they live in Bookmarks grove right at the coast of the Semantics, a large ocean regelialia.",
-    },
-];
-
-const cardEl = document.querySelector(".cards");
-let cardDOM = "";
-
-for (let i = 0; i < infos.length; i++) {
-    cardDOM += `
-        <div class="card">
-            <div class="icon">
-                <i class="fa-solid ${infos[i].icon} fa-3x"></i>
-            </div>
-            <p class="title">${infos[i].name}</p>
-            <p class="desc">${infos[i].desc}</p>
-        </div>
-    `;
-
-    cardEl.innerHTML = cardDOM;
-}
-
-// Navigation
-
 const scrollLinks = document.querySelectorAll(".scroll-link");
 
 scrollLinks.forEach(function (link) {
@@ -69,10 +32,7 @@ colors.forEach(function (color) {
     color.addEventListener("click", function () {
         const style = getComputedStyle(color);
         const bgColor = `${style.backgroundColor}`;
-        console.log(bgColor);
-
-        body.style.backgroundColor = bgColor;
-        // console.log(nameOfclass);
-        // nameOfclass.style.backgroundColor;
+        document.documentElement.style.setProperty('--clr-gradient', bgColor)
+        document.documentElement.style.setProperty('--clr-cyan', bgColor);
     });
 });
