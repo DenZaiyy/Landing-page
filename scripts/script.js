08362ce7d7b7062cbcb679bd2af42e3282eb398a
DenZaiyy <denzaiyy@outlook.fr>
2023-02-27 17:44:06 +0100

    End animation for settings & adding mobile version

--- scripts/script.js
+++ scripts/script.js
@@ -16,23 +16,30 @@ scrollLinks.forEach(function (link) {
 
 // Option toggle colors
 const gearOption = document.querySelector(".square-option");
+const option = document.querySelector(".options");
 
 gearOption.addEventListener("click", function () {
-    const option = document.querySelector(".options");
-
     option.classList.toggle("active");
+    gearOption.classList.toggle("active");
 });
 
 // Testing colors change
-const body = document.querySelector("body");
-
 const colors = document.querySelectorAll(".color");
 
 colors.forEach(function (color) {
     color.addEventListener("click", function () {
         const style = getComputedStyle(color);
         const bgColor = `${style.backgroundColor}`;
-        document.documentElement.style.setProperty('--clr-gradient', bgColor)
-        document.documentElement.style.setProperty('--clr-cyan', bgColor);
+        document.documentElement.style.setProperty("--clr-gradient", bgColor);
+        document.documentElement.style.setProperty("--clr-cyan", bgColor);
     });
+});
+
+//Burger menu
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+    links.classList.toggle("show-links");
 });
